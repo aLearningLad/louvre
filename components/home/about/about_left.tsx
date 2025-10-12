be4589cec3e949ec09c03aac9088c1070ce0d2a5
aLearningLad <mocmanca@gmail.com>
2025-10-13 01:10:28 +0200

    about section fleshing out

--- components/home/about/about_left.tsx
+++ components/home/about/about_left.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { GiGears } from "react-icons/gi";
+import { FaCubesStacked } from "react-icons/fa6";
 
 const AboutLeft = () => {
   return (
@@ -16,12 +19,60 @@ const AboutLeft = () => {
         </h2>
       </div>
 
-      <div className=" flex flex-col items-start">
-        <h1>Curious?</h1>
-        <div className=" mt-5 flex flex-col w-full">
-          <span className=" w-7/12 flex justify-between h-6 border-b-[1px] border-neutral-300 ">
-            <p className=" text-[10px] ">Contact me</p>
-            <p className="text-[10px]">{">"}</p>
+      <div className=" flex flex-col items-start mt-12">
+        <h1 className="text-[10px]">Curious?</h1>
+        <div className=" flex flex-col w-full mt-2">
+          <button className=" w-7/12 cursor-pointer group flex justify-between items-center h-6 hover:pl-5 hover:border-none hover:h-12 hover:bg-neutral-200/10 hover:rounded-lg border-b-[1px] border-neutral-300 ">
+            <p className=" text-[10px] flex items-center gap-2 group-hover:text-[14px] transition-all duration-300 ease-in-out ">
+              Contact me{" "}
+              <MdEmail
+                color="white"
+                size={16}
+                className=" group-hover:flex hidden  "
+              />
+            </p>
+            <p className="text-[10px] group-hover:hidden transition-all duration-300 ease-in-out ">
+              {">"}
+            </p>
+          </button>
+          <span className=" w-7/12 mt-8 cursor-pointer group flex justify-between items-center h-6 hover:pl-5 hover:border-none hover:h-12 hover:bg-neutral-200/10 hover:rounded-lg border-b-[1px] border-neutral-300 ">
+            <p className=" text-[10px] flex items-center gap-2 group-hover:text-[14px] transition-all duration-300 ease-in-out ">
+              Browse my Github{" "}
+              <FaGithub
+                size={16}
+                color="white"
+                className=" group-hover:flex hidden "
+              />
+            </p>
+            <p className="text-[10px] group-hover:hidden transition-all duration-300 ease-in-out ">
+              {">"}
+            </p>
+          </span>
+          <span className=" w-7/12 mt-8 cursor-pointer group flex justify-between items-center h-6 hover:pl-5 hover:border-none hover:h-12 hover:bg-neutral-200/10 hover:rounded-lg border-b-[1px] border-neutral-300 ">
+            <p className=" text-[10px] flex items-center gap-2 group-hover:text-[14px] transition-all duration-300 ease-in-out ">
+              Projects
+              <GiGears
+                size={16}
+                color="white"
+                className=" group-hover:flex hidden "
+              />
+            </p>
+            <p className="text-[10px] group-hover:hidden transition-all duration-300 ease-in-out ">
+              {">"}
+            </p>
+          </span>
+          <span className=" w-7/12 mt-8 cursor-pointer group flex justify-between items-center h-6 hover:pl-5 hover:border-none hover:h-12 hover:bg-neutral-200/10 hover:rounded-lg border-b-[1px] border-neutral-300 ">
+            <p className=" text-[10px] flex items-center gap-2 group-hover:text-[14px] transition-all duration-300 ease-in-out ">
+              Tech stack
+              <FaCubesStacked
+                size={16}
+                color="white"
+                className=" group-hover:flex hidden "
+              />
+            </p>
+            <p className="text-[10px] group-hover:hidden transition-all duration-300 ease-in-out ">
+              {">"}
+            </p>
           </span>
         </div>
       </div>
