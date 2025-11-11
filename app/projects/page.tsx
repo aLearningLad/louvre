fd4f817e7178afaa311cb28002434ec61f60a8d4
aLearningLad <mocmanca@gmail.com>
2025-11-11 16:16:02 +0200

    scrollbar tweak with tailwind

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,7 +9,7 @@ import React from "react";
 
 const ProjectsPage = () => {
   return (
-    <div className=" relative w-full h-screen bg-gradient-to-br from-black via-slate-900 to-black overflow-y-scroll snap-y snap-mandatory p-3 lg:p-0 flex flex-col">
+    <div className=" overflow-hidden relative w-full h-screen bg-gradient-to-br from-black via-slate-900 to-black overflow-y-scroll snap-y snap-mandatory p-3 lg:p-0 flex flex-col">
       <Platamigos />
       <EventStream />
       <RoadWalks />
@@ -18,6 +18,8 @@ const ProjectsPage = () => {
 
       {/* bubbles */}
       <div className="projects_bubble absolute w-[200px] h-[200px] rounded-full bg-gradient-to-tr from-pink-600/40 via-pink-600/20 to-transparent " />
+      <div className="projects_bubble absolute bottom-12 right-7 w-[500px] h-[500px] rounded-full bg-gradient-to-bl from-cyan-600/40 via-cyan-600/20 to-transparent " />
+      <div className="projects_bubble absolute top-20 right-32 w-[120px] h-[120px] rounded-full bg-gradient-to-bl from-cyan-600/30 via-cyan-600/10 to-transparent " />
     </div>
   );
 };
